dafef2c709e47b8bb4c79b2ae0906b8182134358
Doridian <git@doridian.net>
2024-08-08 19:37:31 -0700

    fix graph logs

--- src/index.ts
+++ src/index.ts
@@ -17,10 +17,7 @@ async function main(): Promise<void> {
     const startUser = process.env.FA_GRAPH_START ?? 'doridian';
 
     const fa = new Client(new RawAPI(process.env.FA_COOKIE_A, process.env.FA_COOKIE_B));
-    const img = await fa.getSubmission('9380872');
-    console.log(img);
 
-    throw new Error('no');
     const graph = await buildUserGraph(fa, startUser, 2);
     console.log(graph);
     console.log(graph.size);
@@ -36,10 +33,12 @@ async function buildUserGraph(FA: Client, startUser: string, maxDepth: number): 
     const queue: IUserGraphQueueEntry[] = [];
     const visited = new Set<string>();
 
-    console.log(`Start user ${startUser} is watching users`);
+    let i = 0;
     for await (const user of FA.getWatching(startUser)) {
         queue.push({ id: user.id, depth: 1, raw: user });
+        i++;
     }
+    console.log(`Start user ${startUser} is watching ${i} users`);
 
     while (queue.length > 0) {
         const entry = queue.shift();
@@ -58,10 +57,12 @@ async function buildUserGraph(FA: Client, startUser: string, maxDepth: number): 
         }
 
         console.log('Checking user', entry.id, 'at depth', entry.depth);
-        console.log(`User ${entry.id} is watching users`);
+        i = 0;
         for await (const user of FA.getWatching(entry.id)) {
             queue.push({ id: user.id, raw: user, depth: entry.depth + 1 });
+            i++;
         }
+        console.log(`User ${entry.id} is watching ${i} users`);
     }
 
     return visited;
