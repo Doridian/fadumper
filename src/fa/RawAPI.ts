c5174ef3708d0c5519c1412f26bfd79d1f5857a3
Mark Dietzer <git@doridian.net>
2024-09-02 12:54:32 -0700

    Rename timeout again (#95)

--- src/fa/RawAPI.ts
+++ src/fa/RawAPI.ts
@@ -17,7 +17,7 @@ const httpsAgent = process.env.PROXY_URL
     : new Agent(httpAgentOptions);
 
 const HTTP_RETRIES = Number.parseInt(process.env.HTTP_RETRIES ?? '3', 10);
-const HTTP_CONNECT_TIMEOUT = Number.parseInt(process.env.HTTP_CONNECT_TIMEOUT ?? '10000', 10);
+const HTTP_ROUND_TRIP_TIMEOUT = Number.parseInt(process.env.HTTP_ROUND_TRIP_TIMEOUT ?? '10000', 10);
 const HTTP_BODY_TIMEOUT = Number.parseInt(process.env.HTTP_BODY_TIMEOUT ?? '10000', 10);
 
 const ERROR_UNKNOWN = new Error('Unknown error, this should not happen');
@@ -258,10 +258,10 @@ export class RawAPI {
                 .end();
 
             reqTimeout = setTimeout(() => {
-                const err = new Error('Connect timeout');
+                const err = new Error('Round trip timeout');
                 req.destroy(err);
                 reject(err);
-            }, HTTP_CONNECT_TIMEOUT);
+            }, HTTP_ROUND_TRIP_TIMEOUT);
         });
     }
 }
