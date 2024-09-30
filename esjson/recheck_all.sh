35e47530278dc1c3fb5f6e7791a4a2b5bdcf0528
Mark Dietzer <git@doridian.net>
2024-09-30 14:57:37 -0700

    Improve query to re-set downloaded flag (#148)

--- esjson/recheck_all.sh
+++ esjson/recheck_all.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 curl -v -XPOST -H 'Content-Type: application/json' 'http://elasticsearch:9200/fa_submissions/_update_by_query?conflicts=proceed' --data-raw '{
+    "query": {
+        "bool": {
+            "must": [
+                {
+                    "term": {
+                        "downloaded": true
+                    }
+                }
+            ]
+        }
+    },
     "script":
     {
         "source": "ctx._source.downloaded = false;",
@@ -7,6 +18,17 @@ curl -v -XPOST -H 'Content-Type: application/json' 'http://elasticsearch:9200/fa
     }
 }'
 curl -v -XPOST -H 'Content-Type: application/json' 'http://elasticsearch:9200/fa_users/_update_by_query?conflicts=proceed' --data-raw '{
+    "query": {
+        "bool": {
+            "must": [
+                {
+                    "term": {
+                        "downloaded": true
+                    }
+                }
+            ]
+        }
+    },
     "script":
     {
         "source": "ctx._source.downloaded = false;",
