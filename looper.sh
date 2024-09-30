1fdf93604c7cea36556eced8f14f4519d50f022f
Mark Dietzer <git@doridian.net>
2024-09-30 13:27:08 -0700

    Allow passing extra args (#147)

--- looper.sh
+++ looper.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
+set -x
+
 export PROXY_URL="${LOOPER_FETCHNEW_PROXY_URL-}"
 
 echo 'Fetch'
-until node dist/bin/fetchnew.js
+until node dist/bin/fetchnew.js ${LOOPER_FETCHNEW_EXTRA_ARGS-}
 do
 	echo 'Retrying fetch'
 	sleep 10
 done
+echo 'Done fetch'
 sleep 10
 
 export PROXY_URL="${LOOPER_DOWNLOADFILES_PROXY_URL-}"
 
 echo 'DL submission'
-until node dist/bin/downloadfiles.js --type=submission --looper
+until node dist/bin/downloadfiles.js --type=submission --looper ${LOOPER_DOWNLOADFILES_EXTRA_ARGS-}
 do
 	echo 'Retrying DL submission'
 	sleep 10
 done
+echo 'Done DL submission'
 sleep 10
 
 echo 'DL user'
-until node dist/bin/downloadfiles.js --type=user --looper
+until node dist/bin/downloadfiles.js --type=user --looper ${LOOPER_DOWNLOADFILES_EXTRA_ARGS-}
 do
 	echo 'Retrying DL user'
 	sleep 10
 done
+echo 'Done DL user'
 sleep 10
