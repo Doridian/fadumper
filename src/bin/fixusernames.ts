5ab5daec7a8c0caaabe7c3f37fa64e25821f1901
Doridian <git@doridian.net>
2024-09-30 22:23:46 -0700

    Fix fixusernames by searching repeatedly instead of scrolling

--- src/bin/fixusernames.ts
+++ src/bin/fixusernames.ts
@@ -12,7 +12,7 @@ configDotenv();
 const client = new ESClient({
     node: process.env.ES_URL,
 });
-const ES_BATCH_SIZE = 1000;
+const ES_BATCH_SIZE = 100;
 
 const faRawAPI = new RawAPI(process.env.FA_COOKIE_A, process.env.FA_COOKIE_B);
 const faClient = new Client(faRawAPI);
@@ -67,35 +67,30 @@ async function getMoreUntilDone(response: SearchResponse): Promise<boolean> {
         handledUsernames.add(typedHit._source.createdBy);
     }
 
-    return false;
+    return response.hits.hits.length > 0;
 }
 
 async function main() {
-    let response = await client.search({
-        index: 'fa_submissions',
-        scroll: '60s',
-        body: {
-            size: ES_BATCH_SIZE,
-            query: {
-                bool: {
-                    must: {
-                        script: {
-                            script: "doc['createdBy'].value.length() != doc['createdByUsername'].value.length()",
+    let response: SearchResponse;
+    do {
+        // eslint-disable-next-line no-await-in-loop
+        response = await client.search({
+            index: 'fa_submissions',
+            body: {
+                size: ES_BATCH_SIZE,
+                query: {
+                    bool: {
+                        must: {
+                            script: {
+                                script: "doc['createdBy'].value.length() != doc['createdByUsername'].value.length()",
+                            },
                         },
                     },
                 },
             },
-        },
-    });
-
-    // eslint-disable-next-line no-await-in-loop
-    while (await getMoreUntilDone(response)) {
-        // eslint-disable-next-line no-await-in-loop
-        response = await client.scroll({
-            scroll_id: response._scroll_id,
-            scroll: '60s',
         });
-    }
+        // eslint-disable-next-line no-await-in-loop
+    } while (await getMoreUntilDone(response));
 }
 
 await main();
