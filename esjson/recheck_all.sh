ea2b74c79ec4df0c680cdfad93227e4ce37ecb0b
Mark Dietzer <git@doridian.net>
2024-10-03 09:18:01 -0700

    Recheck all means recheck all (#181)

--- esjson/recheck_all.sh
+++ esjson/recheck_all.sh
@@ -17,6 +17,26 @@ curl -v -XPOST -H 'Content-Type: application/json' 'http://elasticsearch:9200/fa
         "lang": "painless"
     }
 }'
+
+curl -v -XPOST -H 'Content-Type: application/json' 'http://elasticsearch:9200/fa_submissions/_update_by_query?conflicts=proceed' --data-raw '{
+    "query": {
+        "bool": {
+            "must": [
+                {
+                    "term": {
+                        "deleted": true
+                    }
+                }
+            ]
+        }
+    },
+    "script":
+    {
+        "source": "ctx._source.deleted = false;",
+        "lang": "painless"
+    }
+}'
+
 curl -v -XPOST -H 'Content-Type: application/json' 'http://elasticsearch:9200/fa_users/_update_by_query?conflicts=proceed' --data-raw '{
     "query": {
         "bool": {
@@ -32,6 +52,25 @@ curl -v -XPOST -H 'Content-Type: application/json' 'http://elasticsearch:9200/fa
     "script":
     {
         "source": "ctx._source.downloaded = false;",
+        "lang": "painless"
+    }
+}'
+
+curl -v -XPOST -H 'Content-Type: application/json' 'http://elasticsearch:9200/fa_users/_update_by_query?conflicts=proceed' --data-raw '{
+    "query": {
+        "bool": {
+            "must": [
+                {
+                    "term": {
+                        "deleted": true
+                    }
+                }
+            ]
+        }
+    },
+    "script":
+    {
+        "source": "ctx._source.deleted = false;",
         "lang": "painless"
     }
 }'
