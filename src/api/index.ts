9e63e9bd8eed4c2781cdbf868bbffd052be8de06
Doridian <git@doridian.net>
2024-10-07 21:10:34 -0700

    Passthru /files

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,6 +4,7 @@ import { URL } from 'node:url';
 import { Client as ESClient } from '@elastic/elasticsearch';
 import { SearchHit } from '@elastic/elasticsearch/lib/api/types';
 import express from 'express';
+import { DOWNLOAD_PATH } from '../fa/Downloadable.js';
 import { logger } from '../lib/log.js';
 import { makeHashPath } from '../lib/utils.js';
 
@@ -11,6 +12,8 @@ const app = express();
 const client = new ESClient({
     node: process.env.ES_URL,
 });
+
+app.use('/files', express.static(path.join(DOWNLOAD_PATH, 'hashes')));
 
 app.use(express.text({ type: '*/*' }));
 
