630f8c3496b6d7a8119cb775260ede85dbe9356f
Mark Dietzer <git@doridian.net>
2024-10-08 21:02:31 -0700

    Add title to images (#201)

--- ui/script.js
+++ ui/script.js
@@ -25,6 +25,7 @@ function renderResult(hit) {
             const imgThumbnail = document.createElement('img');
             imgThumbnail.src = hit._source.image;
             imgThumbnail.width = 320;
+            imgThumbnail.title = hit._source.description;
             link.appendChild(imgThumbnail);
             break;
         default:
