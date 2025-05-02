a30d7ac6e7e57638f835bfd00ea4090ce9d9b0be
Mark Dietzer <git@doridian.net>
2025-05-02 16:09:09 -0700

    Add `f` flag to `curl` (#388)

--- esjson/reindex.sh
+++ esjson/reindex.sh
@@ -14,11 +14,11 @@ if [ -z "${NEWVER}" ]; then
 	exit 1
 fi
 
-curl -v -XPUT -H 'Content-Type: application/json' "http://elasticsearch:9200/fa_submissions_${NEWVER}" --data @fa_submissions.json
-curl -v -XPUT -H 'Content-Type: application/json' "http://elasticsearch:9200/fa_users_${NEWVER}" --data @fa_users.json
-curl -v -XPUT -H 'Content-Type: application/json' "http://elasticsearch:9200/fa_journals_${NEWVER}" --data @fa_journals.json
+curl -f -v -XPUT -H 'Content-Type: application/json' "http://elasticsearch:9200/fa_submissions_${NEWVER}" --data @fa_submissions.json
+curl -f -v -XPUT -H 'Content-Type: application/json' "http://elasticsearch:9200/fa_users_${NEWVER}" --data @fa_users.json
+curl -f -v -XPUT -H 'Content-Type: application/json' "http://elasticsearch:9200/fa_journals_${NEWVER}" --data @fa_journals.json
 
-curl -v -XPOST 'http://elasticsearch:9200/_reindex' -H 'Content-Type: application/json' --data-raw "{
+curl -f -v -XPOST 'http://elasticsearch:9200/_reindex' -H 'Content-Type: application/json' --data-raw "{
   \"source\": {
     \"index\": \"fa_submissions_${OLDVER}\"
   },
@@ -28,7 +28,7 @@ curl -v -XPOST 'http://elasticsearch:9200/_reindex' -H 'Content-Type: applicatio
 }"
 
 
-curl -v -XPOST 'http://elasticsearch:9200/_reindex' -H 'Content-Type: application/json' --data-raw "{
+curl -f -v -XPOST 'http://elasticsearch:9200/_reindex' -H 'Content-Type: application/json' --data-raw "{
   \"source\": {
     \"index\": \"fa_users_${OLDVER}\"
   },
@@ -37,7 +37,7 @@ curl -v -XPOST 'http://elasticsearch:9200/_reindex' -H 'Content-Type: applicatio
   }
 }"
 
-curl -v -XPOST 'http://elasticsearch:9200/_reindex' -H 'Content-Type: application/json' --data-raw "{
+curl -f -v -XPOST 'http://elasticsearch:9200/_reindex' -H 'Content-Type: application/json' --data-raw "{
   \"source\": {
     \"index\": \"fa_journals_${OLDVER}\"
   },
@@ -46,7 +46,7 @@ curl -v -XPOST 'http://elasticsearch:9200/_reindex' -H 'Content-Type: applicatio
   }
 }"
 
-curl -v -XPOST 'http://elasticsearch:9200/_aliases' -H 'Content-Type: application/json' --data-raw "{
+curl -f -v -XPOST 'http://elasticsearch:9200/_aliases' -H 'Content-Type: application/json' --data-raw "{
     \"actions\" : [
         { \"add\": { \"index\": \"fa_journals_${NEWVER}\", \"alias\": \"fa_journals\" } },
         { \"add\": { \"index\": \"fa_users_${NEWVER}\", \"alias\": \"fa_users\" } },
