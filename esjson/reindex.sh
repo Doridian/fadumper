598103ab0afe97cca12caf9dd3f49fbe59b32f44
Doridian <git@doridian.net>
2025-05-02 12:57:54 -0700

    Add reindex.sh

---
+++ esjson/reindex.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Make sure to change the numbers below!
+
+curl -v -XPOST "$(hostname):9200/_reindex" -H 'Content-Type: application/json' --data-raw '{
+  "source": {
+    "index": "fa_submissions_1"
+  },
+  "dest": {
+    "index": "fa_submissions_2"
+  }
+}'
+
+
+curl -v -XPOST "$(hostname):9200/_reindex" -H 'Content-Type: application/json' --data-raw '{
+  "source": {
+    "index": "fa_users_1"
+  },
+  "dest": {
+    "index": "fa_users_2"
+  }
+}'
+
+curl -v -XPOST "$(hostname):9200/_reindex" -H 'Content-Type: application/json' --data-raw '{
+  "source": {
+    "index": "fa_journals_1"
+  },
+  "dest": {
+    "index": "fa_journals_2"
+  }
+}'
+
+curl -v -XPOST "$(hostname):9200/_aliases" -H 'Content-Type: application/json' --data-raw '{
+    "actions" : [
+        { "add" : { "index" : "fa_journals_2", "alias" : "fa_journals" } },
+        { "add" : { "index" : "fa_users_2", "alias" : "fa_users" } },
+        { "add" : { "index" : "fa_submissions_2", "alias" : "fa_users" } }
+    ]
+}'
