46c4e0667f66ef0481fd8bee75420b228cffc37d
Doridian <git@doridian.net>
2025-05-02 13:18:24 -0700

    fixes

--- esjson/reindex.sh
+++ esjson/reindex.sh
@@ -5,41 +5,41 @@ OLDVER=1
 NEWVER=2
 
 curl -v -XPOST 'http://elasticsearch:9200/_reindex' -H 'Content-Type: application/json' --data-raw "{
-  "source": {
-    "index": \"fa_submissions_${OLDVER}\"
+  \"source\": {
+    \"index\": \"fa_submissions_${OLDVER}\"
   },
-  "dest": {
-    "index": \"fa_submissions_${NEWVER}\"
+  \"dest\": {
+    \"index\": \"fa_submissions_${NEWVER}\"
   }
 }"
 
 
 curl -v -XPOST 'http://elasticsearch:9200/_reindex' -H 'Content-Type: application/json' --data-raw "{
-  "source": {
-    "index": \"fa_users_${OLDVER}\"
+  \"source\": {
+    \"index\": \"fa_users_${OLDVER}\"
   },
-  "dest": {
-    "index": \"fa_users_${NEWVER}\"
+  \"dest\": {
+    \"index\": \"fa_users_${NEWVER}\"
   }
 }"
 
 curl -v -XPOST 'http://elasticsearch:9200/_reindex' -H 'Content-Type: application/json' --data-raw "{
-  "source": {
-    "index": \"fa_journals_${OLDVER}\"
+  \"source\": {
+    \"index\": \"fa_journals_${OLDVER}\"
   },
-  "dest": {
-    "index": \"fa_journals_${NEWVER}\"
+  \"dest\": {
+    \"index\": \"fa_journals_${NEWVER}\"
   }
 }"
 
 curl -v -XPOST 'http://elasticsearch:9200/_aliases' -H 'Content-Type: application/json' --data-raw "{
     \"actions\" : [
-        { \"add\" : { \"index\" : \"fa_journals_${NEWVER}\", \"alias\" : \"fa_journals\" } },
-        { \"add\" : { \"index\" : \"fa_users_${NEWVER}\", \"alias\" : \"fa_users\" } },
-        { \"add\" : { \"index\" : \"fa_submissions_${NEWVER}\", \"alias\" : \"fa_submissions\" } },
-        { \"remove\" : { \"index\" : \"fa_journals_${OLDVER}\", \"alias\" : \"fa_journals\" } },
-        { \"remove\" : { \"index\" : \"fa_users_${OLDVER}\", \"alias\" : \"fa_users\" } },
-        { \"remove\" : { \"index\" : \"fa_submissions_${OLDVER}\", \"alias\" : \"fa_submissions\" } }
+        { \"add\": { \"index\": \"fa_journals_${NEWVER}\", \"alias\": \"fa_journals\" } },
+        { \"add\": { \"index\": \"fa_users_${NEWVER}\", \"alias\": \"fa_users\" } },
+        { \"add\": { \"index\": \"fa_submissions_${NEWVER}\", \"alias\": \"fa_submissions\" } },
+        { \"remove\": { \"index\": \"fa_journals_${OLDVER}\", \"alias\": \"fa_journals\" } },
+        { \"remove\": { \"index\": \"fa_users_${OLDVER}\", \"alias\": \"fa_users\" } },
+        { \"remove\": { \"index\": \"fa_submissions_${OLDVER}\", \"alias\": \"fa_submissions\" } }
     ]
 }"
 
