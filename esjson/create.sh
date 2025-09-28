d6e916a8c61b8f76432c05b6685ddde8750bb0f5
Mark Dietzer <git@doridian.net>
2025-09-28 16:09:25 -0700

    Fix URLs (#524)

--- esjson/create.sh
+++ esjson/create.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-curl -v -XPUT -H 'Content-Type: application/json' 'http://elasticsearch:9200/fa_submissions_1' --data @fa_submissions.json
-curl -v -XPUT -H 'Content-Type: application/json' 'http://elasticsearch:9200/fa_users_1' --data @fa_users.json
-curl -v -XPUT -H 'Content-Type: application/json' 'http://elasticsearch:9200/fa_journals_1' --data @fa_journals.json
+curl -v -XPUT -H 'Content-Type: application/json' 'http://opensearch:9200/fa_submissions_1' --data @fa_submissions.json
+curl -v -XPUT -H 'Content-Type: application/json' 'http://opensearch:9200/fa_users_1' --data @fa_users.json
+curl -v -XPUT -H 'Content-Type: application/json' 'http://opensearch:9200/fa_journals_1' --data @fa_journals.json
 
-curl -v -XPOST -H 'Content-Type: application/json' 'http://elasticsearch:9200/_aliases' --data @aliases.json
+curl -v -XPOST -H 'Content-Type: application/json' 'http://opensearch:9200/_aliases' --data @aliases.json
