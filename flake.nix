d4267ffed1365f3685026debf4de4fa1b67f86be
Doridian <git@doridian.net>
2025-09-30 22:14:54 -0700

    no more manual hash

--- flake.nix
+++ flake.nix
@@ -14,7 +14,8 @@
             pname = "fadumper";
             version = "1.0.0";
             src = ./.;
-            npmDepsHash = "sha256-rdg9jc8l6pvdYtX6WpFkku/CQKLEmTPzCieR+Rjwi5Y=";
+            npmDeps = pkgs.importNpmLock { npmRoot = ./.; };
+            npmConfigHook = pkgs.importNpmLock.npmConfigHook;
           };
         };
       });
