91de68bf2ed2e6f96f5a8bdd1b13cd18ec58596f
Doridian <git@doridian.net>
2025-10-03 11:55:16 -0700

    Named package

--- flake.nix
+++ flake.nix
@@ -7,16 +7,18 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        package = pkgs.buildNpmPackage {
+          pname = "fadumper";
+          version = "1.0.0";
+          src = ./.;
+          npmDeps = pkgs.importNpmLock { npmRoot = ./.; };
+          npmConfigHook = pkgs.importNpmLock.npmConfigHook;
+        };
       in
       {
         packages = {
-          default = pkgs.buildNpmPackage {
-            pname = "fadumper";
-            version = "1.0.0";
-            src = ./.;
-            npmDeps = pkgs.importNpmLock { npmRoot = ./.; };
-            npmConfigHook = pkgs.importNpmLock.npmConfigHook;
-          };
+          default = package;
+          fadumper = package;
         };
       });
 }
