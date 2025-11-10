e852c25fea3dd029842d2b8b83ceafea4ed7cfe5
Mark Dietzer <git@doridian.net>
2025-11-09 23:38:54 -0800

    Add binaries (#564)

--- looper.sh
+++ looper.sh
@@ -10,7 +10,7 @@ fi
 export PROXY_URL="${LOOPER_FETCHNEW_PROXY_URL-}"
 
 echo 'Fetch'
-until node dist/bin/fetchnew.js ${LOOPER_FETCHNEW_EXTRA_ARGS-}
+until fadumper-fetchnew ${LOOPER_FETCHNEW_EXTRA_ARGS-}
 do
 	echo 'Retrying fetch'
 	sleep 10
@@ -21,19 +21,10 @@ sleep 10
 export PROXY_URL="${LOOPER_DOWNLOADFILES_PROXY_URL-}"
 
 echo 'DL submission'
-until node dist/bin/downloadfiles.js --type=submission --looper ${LOOPER_DOWNLOADFILES_EXTRA_ARGS-}
+until fadumper-downloadfiles --type=submission --looper ${LOOPER_DOWNLOADFILES_EXTRA_ARGS-}
 do
 	echo 'Retrying DL submission'
 	sleep 10
 done
 echo 'Done DL submission'
-sleep 10
-
-echo 'DL user'
-until node dist/bin/downloadfiles.js --type=user --looper ${LOOPER_DOWNLOADFILES_EXTRA_ARGS-}
-do
-	echo 'Retrying DL user'
-	sleep 10
-done
-echo 'Done DL user'
 sleep 10
